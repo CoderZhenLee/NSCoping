79b7135fc3cec945935dbbdceee199973cdac826
zhenlee <zhenlee@126.com>
2017-06-13 16:34:33 +0800

    Initial Commit

---
+++ NSCoping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSCoping
+//
+//  Created by 李震 on 2017/6/13.
+//  Copyright © 2017年 zhenlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
